cd5a75e1abb5339301ebff016b446fc5365cd79a
Christopher Holmes <holmes.chris94@yahoo.com>
2020-07-15 15:15:07 -0400

    Created package.json, installed techs, required techs, created connection

---
+++ server.js
@@ -0,0 +1,17 @@
+// Require inquirer to prompt user
+const inquirer = require("inquirer");
+
+// Require mySQL to connect to DB
+const mysql = require("mysql");
+
+// Require console.table to print the table to console
+const console = require("console.table");
+
+// Establish the connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "#Wert54321",
+    database: "employeetrackDB"
+  });
